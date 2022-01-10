c5b4057c813ffc6b662c7c5fb6ce9d78d6f8bfcf
Sairaj Harmalkar <sairaj.harmalkar@creativecapsule.com>
2022-01-10 17:43:06 +0530

    Landing-Page-Test: * Worked on the footer for desktop and responsive view. * Replaced the banner image. * Fixed the issues observed.

--- content/scripts/main.js
+++ content/scripts/main.js
@@ -1,11 +1,9 @@
 /* Adding class to body for overlay in mobile menu */
 $('.navbar').on('shown.bs.collapse', function () {
-    // alert('animation complete');
     $('body').addClass('mobile-menu-open');
 })
 
 /* Removing class from body for overlay in mobile menu */
 $('.navbar').on('hidden.bs.collapse', function () {
-    // alert('animation complete');
     $('body').removeClass('mobile-menu-open');
 })
